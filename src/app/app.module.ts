e3d2617d1ddde19acbd0c3dba2b4b2cffba4a2f0
morcar <morcar@americas.corpdir.net>
2022-01-21 21:02:48 -0600

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './modules/test/test.component';
+import { LayoutComponent } from './modules/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
